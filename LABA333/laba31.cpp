e192436d89e881299b44abdbd70bddf042689678
Alina Mananaeva <142620194+sodslx@users.noreply.github.com>
2024-12-13 11:05:36 +0300

    Add files via upload

---
+++ LABA333/laba31.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream> 
+#include <vector> 
+#include <memory> 
+
+using namespace std;
+
+class Detail {
+protected:
+    Detail() {}
+public:
+    virtual ~Detail() {}
+    virtual void show() const = 0;
+};
+
+class Assembly : public Detail {
+protected:
+    Assembly() {}
+public:
+    ~Assembly() override {}
+    void show() const override {
+        cout << "Это сборка." << endl;
+    }
+};
+
+class ConcreteDetail : public Detail {
+protected:
+    ConcreteDetail() {}
+public:
+    ~ConcreteDetail() override {}
+    void show() const override {
+        cout << "Это конкретная деталь." << endl;
+    }
+    static shared_ptr<ConcreteDetail> create() {
+        return shared_ptr<ConcreteDetail>(new ConcreteDetail());
+    }
+};
+
+class ConcreteAssembly : public Assembly {
+protected:
+    ConcreteAssembly() {}
+public:
+    ~ConcreteAssembly() override {}
+    void show() const override {
+        cout << "Это конкретная сборка." << endl;
+    }
+    static shared_ptr<ConcreteAssembly> create() {
+        return shared_ptr<ConcreteAssembly>(new ConcreteAssembly());
+    }
+};
+
+template <typename T>
+void addToStorage(vector<shared_ptr<Detail>>& storage, T element) {
+    storage.push_back(element);
+}
+    int main() {
+        setlocale(LC_ALL, "rus");
+        vector<shared_ptr<Detail>> storage;
+        addToStorage(storage, ConcreteDetail::create());
+        addToStorage(storage, ConcreteAssembly::create());
+        for (const auto& item : storage) {
+            item->show();
+        }
+        return 0;
+    }
---
+++ LABA333/laba32.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream> 
+#include <vector> 
+#include <memory> 
+#include <cstdlib> 
+#include <ctime> 
+
+using namespace std;
+
+class Base {
+protected:
+    Base() {}
+public:
+    virtual ~Base() {}
+    virtual void show() const = 0;
+};
+
+class Derived : public Base {
+protected:
+    Derived() {}
+public:
+    ~Derived() override {}
+    void show() const override {
+        cout << "Это Derived." << endl;
+    }
+
+    static shared_ptr<Derived> create() {
+        return shared_ptr<Derived>(new Derived());
+    }
+};
+
+class ConcreteBase : public Base {
+protected:
+    ConcreteBase() {}
+public:
+    ~ConcreteBase() override {}
+    void show() const override {
+        cout << "Это ConcreteBase." << endl;
+    }
+
+    static shared_ptr<ConcreteBase> create() {
+        return shared_ptr<ConcreteBase>(new ConcreteBase());
+    }
+};
+
+template <typename T>
+void addToStorage(vector<shared_ptr<Base>>& storage, T element) {
+    storage.push_back(element);
+}
+int main() {
+    setlocale(LC_ALL, "rus");
+    srand(static_cast<unsigned int>(time(0)));
+    vector<shared_ptr<Base>> storage;
+    for (int i = 0; i < 10; ++i) {
+        int randomNumber = rand();
+        if (randomNumber % 2 == 0) {
+            addToStorage(storage, ConcreteBase::create());
+        }
+        else {
+            addToStorage(storage, Derived::create());
+        }
+    }
+    for (const auto& item : storage) {
+        item->show();
+    }
+    storage.clear();
+    return 0;
+}
